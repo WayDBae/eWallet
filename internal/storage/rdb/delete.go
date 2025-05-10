b0aacaae81a761bbac34a6a8b4d263960f11dfbf
WayDBae <khayrakov.d@gmail.com>
2025-05-10 19:37:19 -0400

    fix

--- internal/storage/rdb/delete.go
+++ internal/storage/rdb/delete.go
@@ -9,8 +9,7 @@ import (
 func (p *provider) Delete(phone string, ctx context.Context) (err error) {
 	key := "user:" + phone
 
-	result, err := p.client.Del(ctx, key).Result()
-	p.logger.Debug().Err(err).Str("phone", phone).Int64("result", result).Msg("Deleting data")
+	err = p.client.Del(ctx, key).Err()
 	if err != nil {
 		p.logger.Error().Err(err).Str("phone", phone).Ctx(ctx).Msg("An error occurred while trying to deleting data from redis")
 		err = response.ErrInternalServer
